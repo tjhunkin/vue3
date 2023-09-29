6e83ded681bf1a898ac50c7b29c158ebd175b511
tjhunkin-inhance <thomas.hunkin@inhancesc.com>
2023-09-29 14:59:50 +0200

    Episode 7: Bring it All Together

--- js/components/App.js
+++ js/components/App.js
@@ -1,12 +1,14 @@
-import AppButton from "./AppButton.js";
+import Assignments from "./Assignments.js";
 
 export default {
+    components: {Assignments},
+
+    template: `
+      <assignments></assignments>
+    `,
     mounted() {
         setTimeout(() => {
             document.title = 'Episode 7: Bring it All Together';
         }, 1000)
     },
-    components: {
-        'app-button': AppButton,
-    }
 }
