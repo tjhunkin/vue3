d10f9277668ffcd40fabb751827f5364f8c87480
tjhunkin-inhance <thomas.hunkin@inhancesc.com>
2023-09-29 13:57:37 +0200

    Episode 5: One Vue Component Per File

---
+++ js/components/AppButton.js
@@ -0,0 +1,20 @@
+// javascript module: exports a default object
+export default {
+    template: `
+      <button
+          class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed"
+          :disabled="processing"
+      >
+        <slot/>
+      </button>
+    `,
+
+    data() {
+        return {
+            processing: false
+        };
+    },
+    mounted() {
+        alert('Button component mounted')
+    }
+}
